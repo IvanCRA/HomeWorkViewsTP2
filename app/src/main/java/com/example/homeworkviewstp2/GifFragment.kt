627bc6a19b474ae6dcf3af9b2618602c7dd404ee
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-11-23 00:36:10 +0300

    add navigation graph fragments

--- app/src/main/java/com/example/homeworkviewstp2/GifFragment.kt
+++ app/src/main/java/com/example/homeworkviewstp2/GifFragment.kt
@@ -6,17 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GifFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GifFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gif, container, false)
     }
 }
