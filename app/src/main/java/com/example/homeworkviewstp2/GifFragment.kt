ded56edbc2e7c9e0a87161c075fe4636b2f1cb11
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-11-22 23:48:55 +0300

    Add gifFragment and menu

--- app/src/main/java/com/example/homeworkviewstp2/GifFragment.kt
+++ app/src/main/java/com/example/homeworkviewstp2/GifFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class CivitaiFragment : Fragment() {
-
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GifFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GifFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_civitai, container, false)
+        return inflater.inflate(R.layout.fragment_gif, container, false)
     }
 }
